a5022b5b03e38984cad2fbc1b428a7d33dcf945a
Darshit <darshitrakhasia@gmail.com>
2024-03-17 19:11:12 +0530

    Navbar design

--- frontend/src/pages/includes/components/Navbar.tsx
+++ frontend/src/pages/includes/components/Navbar.tsx
@@ -1,40 +1,50 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "../assets/css/Navbar.css";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/link">Link</Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </Link>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><Link className="dropdown-item" to="#">Action</Link></li>
-                <li><Link className="dropdown-item" to="#">Another action</Link></li>
-                <li><hr className="dropdown-divider" /></li>
-                <li><Link className="dropdown-item" to="#">Something else here</Link></li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link disabled" to="#" tabIndex={-1} aria-disabled="true">Disabled</Link>
-            </li>
+return (
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0"> <li className="nav-item mx-3">
+          <Link className="nav-link active text-uppercase font-weight-bold" aria-current="page" to="#">CALENDER</Link>
+        </li>
+        <li className="nav-item mx-3">
+          <Link className="nav-link active text-uppercase font-weight-bold" to="#">ABOUT</Link>
+        </li>
+        <li className="nav-item mx-3">
+          <Link className="nav-link active text-uppercase font-weight-bold" to="#">COMEDIANS</Link>
+        </li>
+        <li className="nav-item mx-3">
+          <Link className="nav-link active text-uppercase font-weight-bold" to="#">LOCATIONS</Link>
+        </li>
+        <li className="nav-item mx-3">
+          <Link className="nav-link active text-uppercase font-weight-bold" to="#">CONTACT</Link>
+        </li>
+        <li className="nav-item mx-3">
+          <Link className="nav-link active text-uppercase font-weight-bold" to="#">FAQ</Link>
+        </li>
+        <li className="nav-item mx-3 dropdown">
+          <Link className="nav-link active dropdown-toggle text-uppercase font-weight-bold" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            MORE
+          </Link>
+          <ul className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+            <li><Link className="dropdown-item" to="#">Login</Link></li>
+            <li><hr className="dropdown-divider bg-light" /></li>
+            <li><Link className="dropdown-item" to="#">Sign Up</Link></li>
           </ul>
-        </div>
-      </div>
-    </nav>
-  );
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+);
 };
 
 export default Navbar;
